751fdf3f4fd278160dd606cece443bad554b479d
Sebastian Camilo Ramirez Betancur <sebastianramirez748@gmail.com>
2022-07-17 18:19:27 -0500

    Entrega ejercicio tema 1

---
+++ src/com/javaopenbootcamp/JavaSintaxis.java
@@ -0,0 +1,34 @@
+package com.javaopenbootcamp;
+
+public class JavaSintaxis {
+
+    public static void main(String[] args) {
+
+        byte by = 1;
+        System.out.println(by);
+
+        short sh = 100;
+        System.out.println(sh);
+
+        int in = 1000;
+        System.out.println(in);
+
+        long lon = 1000000;
+        System.out.println(lon);
+
+        float fl = 10.3f;
+        System.out.println(fl);
+
+        double dou = 123.4;
+        System.out.println(dou);
+
+        char letra = 's';
+        System.out.println(letra);
+
+        String nombre = "Sebastian Ramirez";
+        System.out.println(nombre);
+
+        boolean esDesarrollador = true;
+        System.out.println(esDesarrollador);
+    }
+}
